2127891d9675b4ff7323d8cb9b2712abdcfc2c9d
Jesse <144274309+jguerrero35@users.noreply.github.com>
2024-01-06 15:52:44 -0500

    added chore card component to react app

---
+++ client/components/ChoreCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ChoreCard() {
+  return (
+    <div id="ChoreCard">
+      <h2>Not Started</h2>
+      
+    </div>
+  );
+}
--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CreateChore from '../components/CreateChore.jsx'
+import ChoreCard from '../components/ChoreCard.jsx'
 
 export default function MainContainer () {
     return (
         <div>
             <h1>Choreify!</h1>
             <CreateChore />
+            <ChoreCard />
         </div>
     )
 }
