9ffe0c65f509609f498115ed16933fc8b75ec94f
Diogo de Souza <hello@diogodesouza.com>
2024-01-06 09:47:09 -0500

    closes #6; implements redux toolkit, initializing store and choresSlice. Also loads all chores into the state  when app starts

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App.jsx";
+import store from "./redux/store.js";
 
 // this will first go to the dom, and get the div with the id of "root",
 // and then render the app into that
-const domNode = document.getElementById('root');
+const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 root.render(<App />);
+store.dispatch(fetchChores);
