75ae8bf5b66df1a3db6bdf02e75384bc0bfa1f20
Diogo de Souza <hello@diogodesouza.com>
2024-01-06 15:41:21 -0500

    fixes import error breaking the server in choreifyController

--- server/controllers/choreController.js
+++ server/controllers/choreController.js
@@ -1,28 +1,27 @@
-const db = require('/models/choreifyModels.js');
+const db = require("../models/choreifyModels.js");
 
 const choreController = {};
 
 choreController.getChores = (res, req, next) => {
-  const query = 'SELECT * FROM chores';
-  db.query(query)
-    .then((data) => {
-      if (data.rows) {
-        // eslint-disable-next-line prefer-destructuring
-        res.locals.choreList = data.rows[0];
-        next();
-      } else {
-        next({ err: 'Problem fetching chores from database' });
-      }
-    });
+  const query = "SELECT * FROM chores";
+  db.query(query).then((data) => {
+    if (data.rows) {
+      // eslint-disable-next-line prefer-destructuring
+      res.locals.choreList = data.rows[0];
+      next();
+    } else {
+      next({ err: "Problem fetching chores from database" });
+    }
+  });
 };
 
 choreController.createChore = (req, res, next) => {
-  const query = 'INSERT INTO chores(description, points, status, group_id)'
+  const query = "INSERT INTO chores(description, points, status, group_id)";
   db.query(query);
   // paramaterize the query
   // evaluated result is an object
   // check a specific property to validate insertion success
   // if successful, assign row to res.locals and send that object to reducers
-}
+};
 
 module.exports = choreController;
