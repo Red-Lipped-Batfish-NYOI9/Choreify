69bcd23f56926507e22e03f60d8ce5dc39c41136
Jesse <144274309+jguerrero35@users.noreply.github.com>
2024-01-06 16:16:19 -0500

    added styling and functionality to card component

--- client/components/ChoreCard.jsx
+++ client/components/ChoreCard.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from 'react';
 
 export default function ChoreCard() {
+  const [name, setName] = useState('');
+  const [chore, setChore] = useState('');
+
   return (
     <div id="ChoreCard">
       <h2>Not Started</h2>
-      
+      <p>
+        Chore:
+        {' '}
+        {chore}
+      </p>
+      <p>
+        Owner:
+        {' '}
+        {name}
+      </p>
+      <p>Created Date: "some date from the database"</p>
+      <p>Due Date: </p>
     </div>
   );
 }
