e1e946f12e412d79e72cde93c58a490a5afb5c5b
thomas444ortiz <thomasortiz444@gmail.com>
2024-01-08 12:27:50 -0500

    more styling and fixed merge conflict

--- client/containers/NewGroupContainer.jsx
+++ client/containers/NewGroupContainer.jsx
@@ -7,14 +7,16 @@ export default function NewGroupContainer() {
   // in here, we are going to get the data and then render that into the div
 
   return (
-    <div className={styles.newGroupContainer}>
+    <div>
       <h1>All Groups:</h1>
-      <Group />
-      <Group />
-      <Group />
-      <Group />
-      <Group />
-      <Group />
+      <div className={styles.newGroupContainer}>
+        <Group />
+        <Group />
+        <Group />
+        <Group />
+        <Group />
+        <Group />
+      </div>
       <div>This is abutton that when clicked will redirect to create a new group</div>
     </div>
   );
