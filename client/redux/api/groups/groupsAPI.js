d0fd202b14cc72a2a7a4932a7b8ad9ac857c79d3
thomas444ortiz <thomasortiz444@gmail.com>
2024-01-08 14:19:18 -0500

    connected redux store

--- client/redux/api/groups/groupsAPI.js
+++ client/redux/api/groups/groupsAPI.js
@@ -3,12 +3,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // Define a service using a base URL and expected endpoints
 export const groupsApi = createApi({
   reducerPath: 'groupsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:8080/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/api' }),
   endpoints: (builder) => ({
-    getAllChores: builder.query({
+    getAllGroups: builder.query({
       query: () => '/groups',
     }),
   }),
 });
 
-export const { getAllGroups } = groupsApi;
+export const { useGetAllGroupsQuery } = groupsApi;
--- client/redux/store.js
+++ client/redux/store.js
@@ -1,15 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import choresReducer from "./slices/choresSlice";
+import groupsReducer from "./slices/groupsSlice";
 import { choresApi } from "./api/chores/choresApi";
+import { groupsApi } from "./api/groups/groupsApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     chores: choresReducer,
     [choresApi.reducerPath]: choresApi.reducer, // Add the generated reducer as a specific top-level slice
+    groups: groupsReducer,
+    [groupsApi.reducerPath]: groupsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(choresApi.middleware),
+    getDefaultMiddleware().concat([choresApi.middleware, groupsApi.middleware])
 });
 
 setupListeners(store.dispatch);
