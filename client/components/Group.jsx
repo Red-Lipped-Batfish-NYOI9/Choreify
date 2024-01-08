7d766122ee5ddf8cc4cdd016f5b9f52e8ea759fb
thomas444ortiz <thomasortiz444@gmail.com>
2024-01-08 15:59:55 -0500

    commit

--- client/components/Group.jsx
+++ client/components/Group.jsx
@@ -2,26 +2,19 @@
 import React from 'react';
 import styles from '../stylesheets/Groups.module.css';
 
-export default function Group() {
-  // in here, we are going to make a fetch request to get all groups,
-  // then we are going to render them into the div below
-
+export default function Group(props) {
   return (
     <div className={styles.groupInfoCard}>
-      <h3>Group Name</h3>
+      <h3>{/*props.group_name*/'name'}</h3>
       <div className={styles.columnHolder}>
         <div className={styles.column}>
           Member Name
           <div className={styles.box}>Member 1</div>
           <div className={styles.box}>Member 2</div>
-          <div className={styles.box}>Member 3</div>
-          <div className={styles.box}>Member 4</div>
         </div>
         <div className={styles.column}>
           Member role
           <div className={styles.box}>Admin</div>
-          <div className={styles.box}>User</div>
-          <div className={styles.box}>User</div>
           <div className={styles.box}>User</div>
         </div>
       </div>
--- client/containers/NewGroupContainer.jsx
+++ client/containers/NewGroupContainer.jsx
@@ -8,22 +8,33 @@ export default function NewGroupContainer() {
   // in here, we are going to get the data and then render that into the div
   const { data, error, isLoading } = useGetAllGroupsQuery();
 
-  //console.log(data);
+  console.log('groupData', data);
+
+  const groupsArr = [];
+  /*
+  if (!isLoading) {
+    const groupPlaceholder = {};
+
+    for (let i = 0; i < data.length; i++) {
+      if(!groupPlaceholder.hasOwnProperty(data[i].group_name)){
+        groupPlaceholder[data[i].group_name] = { }
+      }
+    }
+
+
+    for (let i = 0; i < data.length; i++) {
+      groupsArr.push(<Group key={data[i].group_id} group_name={data[i].group_name} />);
+    }
+  }
+  */
 
   return (
     <div>
       <h1>All Groups:</h1>
       <div className={styles.newGroupContainer}>
-        <Group />
-        <Group />
-        <Group />
-        <Group />
-        <Group />
-        <Group />
-        <Group />
-        <Group />
+        {isLoading ? 'Loading...' : groupsArr}
       </div>
-      <div>This is abutton that when clicked will redirect to create a new group</div>
+      <div>This is a button that when clicked will redirect to create a new group</div>
     </div>
   );
 }
