2ae73dc356111c03c2bc36afc1b86eae0bb62164
Wu-Cheney <maxiwu123@gmail.com>
2024-01-11 11:32:57 -0500

    chores render upon page load

--- client/pages/KanbanPage.jsx
+++ client/pages/KanbanPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import MainContainer from "../containers/MainContainer.jsx";
+import { useDispatch } from 'react-redux';
 
 import { fetchChores } from "../redux/slices/choresSlice.js";
 export default function KanbanPage() {
+  
   return (
     <div>
       <h1>Progress Board</h1>
