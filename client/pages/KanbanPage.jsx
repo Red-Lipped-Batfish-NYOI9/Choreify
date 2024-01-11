92b506be7c9148642220a668533120c835db6f20
Wu-Cheney <maxiwu123@gmail.com>
2024-01-11 11:33:42 -0500

    chores page renders chores upon render

--- client/pages/KanbanPage.jsx
+++ client/pages/KanbanPage.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainContainer from "../containers/MainContainer.jsx";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setChoresList } from '../redux/slices/choresSlice.js';
 
 import { fetchChores } from "../redux/slices/choresSlice.js";
 export default function KanbanPage() {
+
+  const dispatch = useDispatch();
+  // const chores = useSelector((state) => state.chores.choreList);
+
+  useEffect(async()=>{
+    fetch('/api/chores')
+    .then(data => data.json())
+    .then(data => {
+      console.log('dispatching data to render chores on kancban', data)
+      dispatch(setChoresList({choreList:data}))})
+  },[])
+
+
+  
   
   return (
     <div>
