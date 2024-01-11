9e9dd0d2ca800695d979221e66a4af461f2778b6
Tyler Savitsky <tsavitsky11@gmail.com>
2024-01-11 16:18:24 -0500

    built out barebones testing for choreController middleware

--- server/controllers/choreController.js
+++ server/controllers/choreController.js
@@ -1,54 +1,38 @@
+const { errorMonitor } = require('events');
 const db = require('../models/choreifyModels');
 const queries = require('../models/queries');
 
 const choreController = {};
 
 choreController.getChores = (req, res, next) => {
-<<<<<<< Updated upstream
-  db.query(queries.getAllChores).then((data) => {
-    if (data) {
-      // eslint-disable-next-line prefer-destructuring
-      res.locals.choreList = data.rows;
-      next();
-    } else {
-      next({ err: 'Problem fetching chores from database' });
-    }
-  });
-=======
   db.query(queries.getAllChores)
     .then((data) => {
       if (data) {
-        // eslint-disable-next-line prefer-destructuring
-        console.log("DATA.ROWS ~~~~~~~~~~~~~~~~", data.rows);
         res.locals.choreList = data.rows;
-        next();
+        return next();
       } else {
-        next({ err: 'Problem fetching chores from database' });
+        return next({ err: 'Problem fetching chores from database' });
       }
+    })
+    .catch((err) => {
+      return next(err);
     });
->>>>>>> Stashed changes
 };
 
 choreController.createChore = (req, res, next) => {
-  // paramaterize the query
-  // console.log(req.body);
-  // const arr = [title, description, group_id, chore_status, due_date, assigner_id,
-  // created_date] = req.body;
-  // console.log(arr);
   const newChoreData = req.body;
-  console.log('reached createChore Controller ');
-  // const arr = ['feed the cat', 'feed the cat her favorite cat food', 1, 'assigned', 'end of the week', 1, (new Date()).toLocaleString('en-US')];
-  // pass the params into the function that will insert them in the query
-  // then insert them into the database using db.query
-  db.query(queries.createChore, newChoreData).then((data) => {
+  db.query(queries.createChore, newChoreData)
+  .then((data) => {
     if (data.rows) {
-      console.log('this is data rows', data.rows);
-      res.locals.newChore = data.rows; // may need to see what this function actually returns
-      next();
+      // res.locals.newChore = data.rows;
+      return next();
     } else {
-      next({ err: 'Problem creating new chore in database' });
+      return next({ err: 'Problem creating new chore in database' });
     }
-  });
+  })
+  .catch((err) => {
+    return next(err);
+  })
 };
 
 module.exports = choreController;
