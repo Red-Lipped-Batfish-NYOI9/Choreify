4a189dce987a8f8e771542675b812f7a1409bb28
Wu-Cheney <maxiwu123@gmail.com>
2024-01-11 15:28:36 -0500

    chorecards can move to the next progress status

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,8 @@
 // require in Express and controllers
 const express = require('express');
 const choreController = require('../controllers/choreController');
-const groupController = require("../controllers/groupController");
-const usersController = require("../controllers/usersController");
+const groupController = require('../controllers/groupController');
+const usersController = require('../controllers/usersController');
 const newGroupController = require('../controllers/newGroupController');
 
 // create router
@@ -15,33 +15,47 @@ router.get('/chores', choreController.getChores, (req, res) => {
 });
 
 // create a route for sending a new chore's data to the database
-router.post('/chores', choreController.createChore, choreController.getChores, (req, res) => {
-  res.status(200).json(res.locals);
-});
+router.post(
+  '/chores',
+  choreController.createChore,
+  choreController.getChores,
+  (req, res) => {
+    res.status(200).json(res.locals);
+  }
+);
 
-router.patch('/chores', choreController.updateChore, (req, res) => {
-  res.status(200).json(res.locals);
-})
+router.patch(
+  '/chores',
+  choreController.updateChore,
+  choreController.getChores,
+  (req, res) => {
+    res.status(200).json(res.locals.choreList);
+  }
+);
 
 // create a rout for getting all group info
-router.get("/groups", groupController.getGroups, (req, res) => {
+router.get('/groups', groupController.getGroups, (req, res) => {
   res.status(200).json(res.locals.groups);
 });
 
 // create a rout for getting all users info
-router.get("/users", usersController.getUsers, (req, res) => {
+router.get('/users', usersController.getUsers, (req, res) => {
   res.status(200).json(res.locals.payload);
 });
 
 // create a rout for posting new user info
-router.post("/users", usersController.postNewUser, (req, res) => {
-  console.log("sending back: ", res.locals.payload);
+router.post('/users', usersController.postNewUser, (req, res) => {
+  console.log('sending back: ', res.locals.payload);
   res.status(200).json(res.locals.payload);
 });
 
 // create a route for new group creations
-router.post('/createNewGroup', newGroupController.createNewGroup, (req, res) => {
-  res.status(200).json(res.locals.newGroup);
-});
+router.post(
+  '/createNewGroup',
+  newGroupController.createNewGroup,
+  (req, res) => {
+    res.status(200).json(res.locals.newGroup);
+  }
+);
 
 module.exports = router;
